870d6bba6d292c2c777f7623ea49916826a3179d
Tobias Kolb <tobster-de@users.noreply.github.com>
2026-02-10 16:24:33 +0100

    Add WPF control with separate package (#64)
    * explicitly target .NET10
    * create presentation package
    * update Readme
    * update assembly versions

---
+++ TwoFactorAuthenticator.Presentation/README.md
@@ -0,0 +1,2 @@
+# TwoFactorAuthenticator.Presentation
+This library contains a control for entering a second factor in Windows Presentation Foundation. 
--- TwoFactorAuthenticator.WpfExample/MainViewModel.cs
+++ TwoFactorAuthenticator.WpfExample/MainViewModel.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows;
-using System.Windows.Input;
 using System.Windows.Media.Imaging;
 using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
@@ -11,54 +10,25 @@ using TwoFactorAuthenticator.Security;
 
 namespace WpfExample
 {
-    public class MainViewModel : ObservableObject
+    public partial class MainViewModel : ObservableObject
     {
+        [ObservableProperty]
         private string _issuer;
 
-        public string Issuer
-        {
-            get => _issuer;
-            set => this.SetProperty(ref _issuer, value);
-        }
-
+        [ObservableProperty]
         private string _account;
 
-        public string Account
-        {
-            get => _account ?? string.Empty;
-            set => this.SetProperty(ref _account, value);
-        }
-
+        [ObservableProperty]
         private string _secret;
 
-        public string Secret
-        {
-            get => _secret ?? string.Empty;
-            set => this.SetProperty(ref _secret, value);
-        }
-
+        [ObservableProperty]
         private string _setupCode;
 
-        public string SetupCode
-        {
-            get => _setupCode ?? string.Empty;
-            set => this.SetProperty(ref _setupCode, value);
-        }
-
+        [ObservableProperty]
         private BitmapSource _qrImageSource;
 
-        public BitmapSource QrImageSource
-        {
-            get => _qrImageSource;
-            set => this.SetProperty(ref _qrImageSource, value);
-        }
-
-        private ICommand _generateCodeCommand;
-
-        public ICommand GenerateCodeCommand =>
-            _generateCodeCommand ??= new RelayCommand(this.ExecuteGenerateCode);
-
-        private void ExecuteGenerateCode()
+        [RelayCommand]
+        private void GenerateCode()
         {
             var tfA = new TwoFactorAuthenticator.Authenticator();
             var qrscg = new QrCoderSetupCodeGenerator();
@@ -78,60 +48,28 @@ namespace WpfExample
             }
         }
 
-        private string _testCode;
+        [ObservableProperty]
+        private PasswordToken _token;
 
-        public string TestCode
-        {
-            get => _testCode;
-            set => this.SetProperty(ref _testCode, value);
-        }
-
-        private ICommand _testCommand;
-
-        public ICommand TestCommand =>
-            _testCommand ??= new RelayCommand(this.ExecuteTestCommand);
-
-        private void ExecuteTestCommand()
+        [RelayCommand]
+        private void Test()
         {
             var tfA = new TwoFactorAuthenticator.Authenticator();
-            bool result = tfA.ValidateTwoFactorPIN(this.Secret, CreateToken(this.TestCode));
+            bool result = tfA.ValidateTwoFactorPIN(this.Secret, this.Token);
 
             MessageBox.Show(result ? "Validated!" : "Incorrect", "Result");
         }
 
-        private static PasswordToken CreateToken(string tokenString)
-        {
-            var token = new PasswordToken(6);
-            foreach (char digit in tokenString)
-            {
-                if (char.IsDigit(digit))
-                {
-                    token.AppendDigit((byte)(digit - '0'));
-                }
-            }
-
-            return token;
-        }
-        
+        [ObservableProperty]
         private string _currentCodes;
 
-        public string CurrentCodes
-        {
-            get => _currentCodes;
-            set => this.SetProperty(ref _currentCodes, value);
-        }
-
-        private ICommand _getCodesCommand;
-
-        public ICommand GetCodesCommand =>
-            _getCodesCommand ??= new RelayCommand(this.ExecuteGetCodesCommand);
-
-        private void ExecuteGetCodesCommand()
+        [RelayCommand]
+        private void GetCodes()
         {
             this.CurrentCodes = string.Join(System.Environment.NewLine,
                 ToReadableText(new TwoFactorAuthenticator.Authenticator().GetCurrentPINs(this.Secret)));
         }
-        
+
         private static IEnumerable<string> ToReadableText(IEnumerable<PasswordToken> currentPins)
         {
             foreach (PasswordToken token in currentPins)
